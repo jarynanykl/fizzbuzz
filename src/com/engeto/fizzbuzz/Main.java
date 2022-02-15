80b2a86729126cc3c5c7364e04af272eaad88994
nyklovajarka <85193356+nyklovajarka@users.noreply.github.com>
2022-02-15 20:26:47 +0100

    Initial commit

---
+++ src/com/engeto/fizzbuzz/Main.java
@@ -0,0 +1,21 @@
+package com.engeto.fizzbuzz;
+
+public class Main {
+
+    static final String FIZZ = "fizz";
+    static final String BUZZ = "buzz";
+    static final String FIZZBUZZ = "fizzbuzz";
+
+    public static void fizzBuzzJava() {
+        for (int i = 1; i <= 100; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) System.out.println((FIZZBUZZ));
+            else if (i % 3 == 0) System.out.println(FIZZ);
+            else if (i % 5 == 0) System.out.println(BUZZ);
+            else System.out.println(i);
+        }
+	}
+
+    public static void main(String[] args) {
+        fizzBuzzJava();
+    }
+}
